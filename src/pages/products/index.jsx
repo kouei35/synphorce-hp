4d389019d82ffe40d3cb50ba466e90b062497886
kouei35 <kouei35@gmail.com>
2024-06-03 18:37:18 +0900

    細かい画像位置の修正

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -15,11 +15,12 @@ export default function Home() {
             <CardHeader className="py-0 px-4 flex-col items-start">
                 <h4 className="font-bold text-large">トライワース株式会社様HP</h4>
                 <p className="text-tiny uppercase font-bold">トライワース株式会社様</p>
-                <CardBody className="overflow-visible py-2">
+                <CardBody className="overflow-visible py-2 flex justify-center items-center">
                     <Image
                     alt="Card background"
                     className="object-cover rounded-xl"
                     src="card/tryworth_hp.png"
+                    width={270}
                     height={180}
                     />
                 </CardBody>
@@ -34,11 +35,12 @@ export default function Home() {
             <CardHeader className="py-0 px-4 flex-col items-start">
                 <h4 className="font-bold text-large">折重様HP</h4>
                 <p className="text-tiny uppercase font-bold">有限会社折重様</p>
-                <CardBody className="overflow-visible py-2">
+                <CardBody className="overflow-visible py-2 flex justify-center items-center">
                     <Image
                     alt="Card background"
                     className="object-cover rounded-xl"
                     src="card/orisige.png"
+                    width={270}
                     height={180}
                     />
                 </CardBody>
@@ -53,11 +55,12 @@ export default function Home() {
             <CardHeader className="py-0 px-4 flex-col items-start">
                 <h4 className="font-bold text-large">家系図アプリ</h4>
                 <p className="text-tiny uppercase font-bold">ライセント株式会社様</p>
-                <CardBody className="overflow-visible py-2">
+                <CardBody className="overflow-visible py-2 flex justify-center items-center">
                     <Image
                     alt="Card background"
                     className="object-cover rounded-xl"
                     src="card/kakeizu.png"
+                    width={270}
                     height={180}
                     />
                 </CardBody>
@@ -72,11 +75,12 @@ export default function Home() {
             <CardHeader className="py-0 px-4 flex-col items-start">
                 <h4 className="font-bold text-large">着せ替えサービス</h4>
                 <p className="text-tiny uppercase font-bold">自社開発</p>
-                <CardBody className="overflow-visible py-2">
+                <CardBody className="overflow-visible py-2 flex justify-center items-center">
                     <Image
                     alt="Card background"
                     className="object-cover rounded-xl"
                     src="card/virtual_try_on.png"
+                    width={270}
                     height={180}
                     />
                 </CardBody>
@@ -89,18 +93,19 @@ export default function Home() {
 
     <h2 class="text-2xl m-2">ゲーム開発</h2>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3gap-7 m-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 m-4">
 
         <div>
             <Card className= "py-2 w-128 m-2">
             <CardHeader className="py-0 px-4 flex-col items-start">
                 <h4 className="font-bold text-large">トライワースアカデミー</h4>
                 <p className="text-tiny uppercase font-bold">トライワース株式会社様</p>
-                <CardBody className="overflow-visible py-2">
+                <CardBody className="overflow-visible py-2 flex justify-center items-center">
                     <Image
                     alt="Card background"
                     className="object-cover rounded-xl"
                     src="card/tryworthAcademy.png"
+                    width={270}
                     height={180}
                     />
                 </CardBody>
