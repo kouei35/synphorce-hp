53f335cfb08ec3f53f8f9afac4c3469615b25ddf
kouei35 <kouei35@gmail.com>
2024-06-04 13:34:51 +0900

    前バージョンのバグ修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from '@chakra-ui/react'
-// import { NextUIProvider } from '@nextui-org/react';
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <NextUIProvider><Component {...pageProps} />;
-// }
 
 import * as React from "react";
 
